f074b86cf360da4298e32aebca41bbee3b557b51
jubair-codes <you@example.com>
2025-04-05 22:20:59 +0600

    marked 36-9 for next

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {Button} from '@mui/material';
 import './App.css';
+import News from './component/News/News';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=9e59d71e1b2c4ce4b1217da6272d7222')
+      .then((response) => response.json())
+      .then((data) => {
+        setArticles(data.articles);
+      })
+      .catch((error) => console.error('Error fetching news:', error));
+  }
+  , []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Top Headlines: {articles.length}</h2>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
     </div>
   );
 }
